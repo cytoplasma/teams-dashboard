eb1d3253b2f97ae3689afe69bb8b4d316046bb55
jeremiah piromgraipakd <jpiromg@gmail.com>
2022-12-08 17:57:55 -0800

    createManager function

--- index.js
+++ index.js
@@ -17,7 +17,59 @@ console.log('\nWelcome to the Team Dashboard Generator!\n');
 
 function generationProcess() {
     function createManager() {
-
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "managerName",
+                message: "What is your manager's name?",
+                validate: (answer) => {
+                    if (answer !== '') {
+                        return true;
+                    } else {
+                        return 'Please enter at least one character!';
+                    };
+                },
+            },
+            {
+                type: "ID",
+                name: "managerID",
+                message: "What is your team manager's ID?",
+                validate: (answer) => {
+                    const pass = answer.match(/^[1-9]\d*$/);
+                    if (pass) {
+                        return true;
+                    } else {
+                        return "Please enter a positive number that is greater than 0";
+                    };
+                },
+            },
+            {
+                type: "input",
+                name: "managerEmail",
+                message: "What is your manager's email?",
+                validate: (answer) => {
+                    const pass = answer.match(/\S+@\S+\.\S+/);
+                    if (pass) {
+                        return true;
+                    } else {
+                        return "Please enter a valid email address";
+                    };
+                },
+            },
+            {
+                type: "input",
+                name: "managerOfficeNumber",
+                message: "What is your manager's office number?",
+                validate: (answer) => {
+                    const pass = answer.match(/^[1-9]\d*$/);
+                    if (pass) {
+                        return true;
+                    } else {
+                        return "Please enter a postive number that is greater than 0";
+                    };
+                },
+            },
+        ])
     }
     function createTeamOptions() {
 
