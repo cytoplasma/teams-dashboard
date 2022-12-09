f2cd38f885fef0429712c3608510f5f300120d3c
jeremiah piromgraipakd <jpiromg@gmail.com>
2022-12-08 20:39:08 -0800

    added jest functionality, and currently bug fixing it using it

--- src/page-template.js
+++ src/page-template.js
@@ -10,7 +10,7 @@ const generateTeam = (team) => {
           </div>
           <div class="card-body">
               <ul class="list-group">
-                  <li class="list-group-item">ID: ${manager.getId()}</li>
+                  <li class="list-group-item">ID: ${manager.getID()}</li>
                   <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
                   <li class="list-group-item">Office number: ${manager.getOfficeNumber()}</li>
               </ul>
@@ -29,7 +29,7 @@ const generateTeam = (team) => {
       </div>
       <div class="card-body">
           <ul class="list-group">
-              <li class="list-group-item">ID: ${engineer.getId()}</li>
+              <li class="list-group-item">ID: ${engineer.getID()}</li>
               <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
               <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.getGithub()}" target="_blank" rel="noopener noreferrer">${engineer.getGithub()}</a></li>
           </ul>
@@ -48,7 +48,7 @@ const generateTeam = (team) => {
       </div>
       <div class="card-body">
           <ul class="list-group">
-              <li class="list-group-item">ID: ${intern.getId()}</li>
+              <li class="list-group-item">ID: ${intern.getID()}</li>
               <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
               <li class="list-group-item">School: ${intern.getSchool()}</li>
           </ul>
